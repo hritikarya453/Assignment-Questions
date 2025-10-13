51126e6589815b4a44de30acdfdf2dc1ad6d4d94
hritikarya453 <hritik.arya.453@gmail.com>
2025-10-14 01:08:17 +0530

    init commit

---
+++ src/ExchangeFourVariables.java
@@ -0,0 +1,33 @@
+public class ExchangeFourVariables {
+    public static void main(String[] args) {
+        // Initial values
+        int W = 1, G = 2, K = 3, A = 4;
+
+        // Display original values
+        System.out.println("Before interchange:");
+        System.out.println("W = " + W + ", G = " + G + ", K = " + K + ", A = " + A);
+
+        // (a) Using an extra variable D
+        int D = W;
+        W = G;
+        G = K;
+        K = A;
+        A = D;
+
+        System.out.println("\nAfter interchange (using extra variable):");
+        System.out.println("W = " + W + ", G = " + G + ", K = " + K + ", A = " + A);
+
+        // Reset values for next method
+        W = 1; G = 2; K = 3; A = 4;
+
+        // (b) Without using extra variable
+        W = W + G + K + A; // W = sum of all
+        A = W - (G + K + A); // A gets original W
+        K = W - (G + K + A); // K gets original A
+        G = W - (G + K + A); // G gets original K
+        W = W - (G + K + A); // W gets original G
+
+        System.out.println("\nAfter interchange (without extra variable):");
+        System.out.println("W = " + W + ", G = " + G + ", K = " + K + ", A = " + A);
+    }
+}
---
+++ src/Hasg1Q2.java
@@ -0,0 +1,30 @@
+public class Hasg1Q2 {
+    public static void main(String[] args) {
+        int a = 2147483647; // Integer.MAX_VALUE
+/*
+        System.out.println("a = " + a);
+        System.out.println("a + 1 = " + (a + 1));
+        System.out.println("2 - a = " + (2 - a));
+        System.out.println("-2 - a = " + (-2 - a));
+        System.out.println("2 * a = " + (2 * a));
+        System.out.println("4 * a = " + (4 * a));
+        System.out.println("3 * a = " + (3 * a));
+        System.out.println("5 * a = " + (5 * a));
+        System.out.println("6 * a = " + (6 * a));
+    }
+}
+    */
+          //Q3
+
+        System.out.println('b');
+        System.out.println('b' + 'c');
+        System.out.println((char) ('a' + 4));
+        //Q4
+        char c = 'A';
+        System.out.println(c);
+        System.out.println((int)c);
+        System.out.println((char)(c + 3));
+
+    }
+}
+
