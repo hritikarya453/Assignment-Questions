c47f1e3267c2f85384314764a9e72caa522e27fa
hritikarya453 <hritik.arya.453@gmail.com>
2025-11-07 00:11:29 +0530

    init commit

--- src/Assignment3/Q08b.java
+++ src/Assignment3/Q08b.java
@@ -1,6 +1,7 @@
 package Assignment3;
 
 import java.util.*;
+
 public class Q08b {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
@@ -9,18 +10,15 @@ public class Q08b {
         double rate;
         if (unit <= 50) {
             rate = unit * 3.00;
-        }
-        else{
+        } else {
             if (unit <= 200) {
                 rate = 50 * 3.00 + (unit - 50) * 4.80;
-            }
-            else{
+            } else {
                 if (unit <= 400) {
                     rate = 50 * 3.00 + 150 * 4.80 + (unit - 200) * 5.80;
-                }
-                else{
+                } else {
                     rate = 50 * 3.00 + 150 * 4.80 + 200 * 5.80 + (unit - 400) * 6.20;
-                    }
+                }
             }
         }
         System.out.println("Do you want to pay your bill online? (Y/N): ");
@@ -29,13 +27,13 @@ public class Q08b {
             case 'Y':
                 double disc = 0.03 * rate;
                 double amount = rate - disc;
-                System.out.println("You received a 3% online payment discount of :" + disc);
-                System.out.println("Total Electricity Bill:" +rate);
-                System.out.println("Total Electricity Bill after discount is :" + amount);
+                System.out.println("You received a 3% online payment discount of : ₹" + disc);
+                System.out.println("Total Electricity Bill : ₹" + rate);
+                System.out.println("Total Electricity Bill after discount is : ₹" + amount);
                 break;
             case 'N':
                 System.out.println("You will get no discount.");
-                System.out.println("Total Electricity Bill is: " + rate);
+                System.out.println("Total Electricity Bill is : ₹" + rate);
                 break;
             default:
                 System.out.println("Invalid Input. Please enter Y or N.");
--- src/Assignment3/Q11.java
+++ src/Assignment3/Q11.java
@@ -3,31 +3,6 @@ package Assignment3;
 import java.util.Scanner;
 
 public class Q11 {
-    static void main(String[] args) {
-        Scanner in = new Scanner(System.in);
-
-        System.out.println("Enter an integer");
-        int a = in.nextInt();
-
-        if(a%6==0||a%5==0) {
-            if (a % 6 == 0 && a % 5 == 0) {
-                System.out.println("Is" + a + "divisible by 5 and 6? true ");
-            } else {
-                System.out.println("Is"+a+" divisible by 5 or 6, but not both? True ");
-            }
-        }
-            else{
-                System.out.println("Is "+ a+" divisible by 5 or 6? true ");
-            }
-
-    }
-}
-
-
-/*
-import java.util.Scanner;
-
-public class DivisibilityCheck {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
 
@@ -45,4 +20,4 @@ public class DivisibilityCheck {
         System.out.println("Is " + number + " divisible by 5 or 6, but not both? " + (divisibleBy5 ^ divisibleBy6));
     }
 }
-*/
+
