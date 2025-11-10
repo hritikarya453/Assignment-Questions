62f57b2d751cd735b17b550a79a26b43a08f9156
hritikarya453 <hritik.arya.453@gmail.com>
2025-11-10 08:58:58 +0530

    init commit

---
+++ src/Assignment4/Q10amw.java
@@ -0,0 +1,21 @@
+package Assignment4;
+
+import java.util.Scanner;
+
+public class Q10amw {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        System.out.println("Enter a number");
+        int n = in.nextInt();
+        String s = String.valueOf(n);
+        String res = "";
+
+        for (int i = 0; i < s.length(); i++) {
+            if (s.charAt(i) == '0') // ✅ compare with character '0'
+                continue;
+            res += s.charAt(i); // append non-zero digits
+        }
+
+        System.out.println("After removing zeroes: " + res);
+    }
+}
