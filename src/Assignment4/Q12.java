7c0152f1de0048ec39ace536176ce03675b57782
hritikarya453 <hritik.arya.453@gmail.com>
2025-11-10 23:04:10 +0530

    init commit

--- src/Assignment4/Q12.java
+++ src/Assignment4/Q12.java
@@ -3,7 +3,7 @@ package Assignment4;
 import java.util.Scanner;
 
 public class Q12 {
-    static void main() {
+    public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
         System.out.println("Enter the base");
         int base = in.nextInt();
---
+++ src/Assignment4/Q14.java
@@ -0,0 +1,25 @@
+package Assignment4;
+
+import java.util.Scanner;
+
+public class Q14 {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        System.out.print("Enter a number: ");
+        int n = in.nextInt();
+
+        int sum = 0;     // Sum of even numbers
+        int prod = 1;    // Product of odd numbers
+
+        for (int i = 1; i <= n; i++) {
+            if (i % 2 == 0) {
+                sum += i;
+            } else {
+                prod *= i;
+            }
+        }
+
+        System.out.println("Sum of all even numbers = " + sum);
+        System.out.println("Product of all odd numbers = " + prod);
+    }
+}
