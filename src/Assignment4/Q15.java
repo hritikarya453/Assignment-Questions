f0354e4e1f0e7f7511c56f58531061413afecbf2
hritikarya453 <hritik.arya.453@gmail.com>
2025-11-14 00:48:37 +0530

    init commit

--- src/Assignment4/Q15.java
+++ src/Assignment4/Q15.java
@@ -4,13 +4,22 @@ import java.util.Scanner;
 
 public class Q15 {
     public static void main(String[] args) {
-        Scanner in = new Scanner (System.in);
-        int sum=0, sq=0;
-        for (int i = 1; i <=100 ; i++) {
-            sum += i*i;
-            sq+=i;
+
+        int n = 100;
+
+        int sumOfSquares = 0;
+        int sum = 0;
+
+        for (int i = 1; i <= n; i++) {
+            sumOfSquares += i * i;  // 1² + 2² + ... + n²
+            sum += i;               // 1 + 2 + ... + n
         }
-        System.out.println("Sum of sq : "+ sum);
-        System.out.println("Square of sum : "+ (int)Math.pow(sq,2));
+
+        int squareOfSum = sum * sum;  // (1 + 2 + ... + n)²
+        int difference = squareOfSum - sumOfSquares;
+
+        System.out.println("Sum of squares: " + sumOfSquares);
+        System.out.println("Square of sum: " + squareOfSum);
+        System.out.println("Difference: " + difference);
     }
 }
