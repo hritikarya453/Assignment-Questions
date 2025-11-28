2686effc284d3ad50e692ef6ca5fd60047f52e3b
hritikarya453 <hritik.arya.453@gmail.com>
2025-11-28 09:52:27 +0530

    init commit

--- src/Assignment4/Q13.java
+++ src/Assignment4/Q13.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
 public class Q13 {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
-        System.out.println("Enter the base");
+        System.out.println("Enter the range");
         int n = in.nextInt();
         int sum = 0;
 
