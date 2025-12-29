4a2e634c2d0ca41028d8bf7536cf923fc3a57b3d
hritikarya453 <hritik.arya.453@gmail.com>
2025-12-29 07:57:37 +0530

    init commit

--- src/Assignment_7/Q_07B_RotateArray.java
+++ src/Assignment_7/Q_07B_RotateArray.java
@@ -3,19 +3,36 @@ package Assignment_7;
 public class Q_07B_RotateArray {
 
     public static void rotateCW(int[] arr) {
-        int n = arr.length - 1;
-        int last = arr[n];
 
-        for (int i = n; i >= 1; i--) {
-            arr[i] = arr[i - 1];
+
+        for (int k = 0; k <3 ; k++) {
+            int n = arr.length - 1;
+            int last = arr[n];
+
+            for (int i = n; i >= 1; i--) {
+                arr[i] = arr[i - 1];
+            }
+            arr[0] = last;
         }
-        arr[0] = last;
+    }
+    public static void rotateACW(int[] arr) {
+
+
+        for (int k = 0; k <1; k++) {
+            int n = arr.length - 1;
+            int first = arr[0];
+
+            for (int i = 0; i <= n-1; i++) {
+                arr[i] = arr[i + 1];
+            }
+            arr[n] = first;
+        }
     }
 
     public static void main(String[] args) {
         int[] arr = {1,2,3,4,5,6,7,8};
 
-        rotateCW(arr);
+        rotateACW(arr);
 
         System.out.print("Rotated Array [ ");
         for (int e : arr) {  // e is element value not index
